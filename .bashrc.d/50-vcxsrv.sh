39525c746db0e9bdbad1a359c7fa08aaaa140183
Stephen Nichols <snichols@nasuni.com>
2022-08-24 11:39:32 -0400

    Made sure DISPLAY set on wsl

--- .bashrc.d/50-vcxsrv.sh
+++ .bashrc.d/50-vcxsrv.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Set up display for VcXsrv if ssh session on Hyper-V/wsl on default internal subnet
+# Set up display for VcXsrv if
+# 1) ssh session on Hyper-V on default internal subnet or
+# 2) on wsl
 VIRT=$(systemd-detect-virt)
-if [ "$SESSION_TYPE" == "remote/ssh" ]; then
-  if [ "$VIRT" == "microsoft" ] || [ "$VIRT" == "wsl" ]; then
-     GW=$(ip route list default | awk '{print $3}')
-     if [ "$(echo $GW | cut -f1 -d.)" == "172" ]; then
-       export LIBGL_ALWAYS_INDIRECT=1
-       export DISPLAY=$GW:0
-     fi
+if ([ "$SESSION_TYPE" == "remote/ssh" ] && [ "$VIRT" == "microsoft" ]) || \
+   [ "$VIRT" == "wsl" ]; then
+  GW=$(ip route list default | awk '{print $3}')
+  if [ "$(echo $GW | cut -f1 -d.)" == "172" ]; then
+    export LIBGL_ALWAYS_INDIRECT=1
+    export DISPLAY=$GW:0
   fi
 fi
