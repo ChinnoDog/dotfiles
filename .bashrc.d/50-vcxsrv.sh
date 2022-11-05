bf3079a32fc8a40eedf5cf4f073d6fe79d2ca756
Stephen Nichols <snichols@nasuni.com>
2022-11-05 02:10:58 +0000

    Updated DISPLAY for new work laptop configuration

--- .bashrc.d/50-vcxsrv.sh
+++ .bashrc.d/50-vcxsrv.sh
@@ -7,10 +7,13 @@ VIRT=$(systemd-detect-virt)
 if ([ "$SESSION_TYPE" == "remote/ssh" ] && [ "$VIRT" == "microsoft" ]) || \
    [ "$VIRT" == "wsl" ]; then
   GW=$(ip route list default | awk '{print $3}')
-  if [ "$(echo $GW | cut -f1 -d.)" == "172" ]; then
+  CLSA=$(echo $GW | cut -f1 -d.)
+  if [ "$CLSA" == "172" ]; then
     GWDIG=$(dig +short -x $GW)
     GWHOST=${GWDIG::-1}
-    export LIBGL_ALWAYS_INDIRECT=1
     export DISPLAY=$GWHOST:0
+  elif [ "$CLSA" == "192" ]; then
+    export DISPLAY=$USER.lan:0
   fi
+  export LIBGL_ALWAYS_INDIRECT=1
 fi
